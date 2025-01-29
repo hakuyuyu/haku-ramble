1f6f7ff4eeff48318a0011a54b5a40b4a6939162
haku yu <fordidofour@gmail.com>
2025-01-29 13:00:00 -0800

    git auto commit

--- loop_mac.sh
+++ loop_mac.sh
@@ -19,15 +19,27 @@ for i in $(seq 1 $1)
 
 do
 
-   export GIT_AUTHOR_DATE="$current_date 13:00:00"
+   # Randomize commit time between 9 AM and 6 PM to avoid GitHub filtering
+   # Generate random hour (9-17) and random minute (0-59)
+   random_hour=$((9 + RANDOM % 9))
+   random_minute=$((RANDOM % 60))
+   random_second=$((RANDOM % 60))
+   
+   # Format time as HH:MM:SS
+   commit_time=$(printf "%02d:%02d:%02d" $random_hour $random_minute $random_second)
 
-   export GIT_COMMITTER_DATE="$current_date 13:00:00"
+   export GIT_AUTHOR_DATE="$current_date $commit_time -0800"
+
+   export GIT_COMMITTER_DATE="$current_date $commit_time -0800"
 
    sh ./push.sh
 
    # Add 1 day (macOS date syntax) - going forwards from 2025-01-02
 
    current_date=$(date -j -v+1d -f "%Y-%m-%d" "$current_date" "+%Y-%m-%d")
+   
+   # Small delay to avoid rate limiting and make it look more natural
+   sleep 1
 
 done
 
